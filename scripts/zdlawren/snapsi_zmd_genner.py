0f3c993f4b0ee2e3c6a5c4b5db578b32c922f766
Zachary Lawrence <4175294+zdlawrence@users.noreply.github.com>
2023-04-25 15:24:09 +0100

    Add docstring explainer and include some pathlib niceties

--- scripts/zdlawren/snapsi_zmd_genner.py
+++ scripts/zdlawren/snapsi_zmd_genner.py
@@ -1,21 +1,48 @@
+""" This python script automatically generates a series of 
+bash scripts for computing zonal mean datasets of the SNAPSI
+data, which can be submitted thru SLURM with sbatch. 
+
+It requires commandline arguments specifying the 
+institution_id (model center) and sub_experiment_id (init date) as
+targets; from there it will generate scripts for every available 
+experiment listed in the SNAPSI intake catalog (nominally, 
+at least 'control', 'nudged', and 'free')
+
+This is sort of a kludgey way to accomplish this task, but I 
+am not very familiar with SLURM and how to pass specific args 
+to scripts.
+
+Original Author: Z. D. Lawrence
+Last modified: 2023-04-25
+"""
+
 import os
+import pathlib
 import argparse
 import intake
 
+USER_HOME = pathlib.Path("~/").expanduser()
+
 # Default locations - can modify these for your setup!
-DEFAULT_OUTPUT_DIR = "/home/users/zdlawren/autoscripts" # where to store the auto-genned bash scripts
-DEFAULT_PYTHON_LOC = "/home/users/zdlawren/mambaforge/envs/py311/bin/python" # absolute path to python 
-DEFAULT_ZMD_LOC = "/home/users/zdlawren/snapsi-diagnostics/scripts/zdlawren/zmd_snapsi.py" # absolute path to the zmd_snapsi.py script
+# The default python location in particular is specific to zdlawren's setup!
+DEFAULT_OUTPUT_DIR = USER_HOME / "autoscripts"
+DEFAULT_PYTHON_LOC = USER_HOME / "mambaforge/envs/py311/bin/python" 
+DEFAULT_ZMD_LOC = USER_HOME / "snapsi-diagnostics/scripts/zdlawren/zmd_snapsi.py"
 
 # Set up the argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("institution", type=str, help="institution_id")
 parser.add_argument("subexperiment", type=str, help="sub_experiment_id")
-parser.add_argument("--outdir", type=str, default=DEFAULT_OUTPUT_DIR)
-parser.add_argument("--python", type=str, default=DEFAULT_PYTHON_LOC)
-parser.add_argument("--zmd", type=str, default=DEFAULT_ZMD_LOC)
+parser.add_argument("--outdir", type=str, default=str(DEFAULT_OUTPUT_DIR))
+parser.add_argument("--python", type=str, default=str(DEFAULT_PYTHON_LOC))
+parser.add_argument("--zmd", type=str, default=str(DEFAULT_ZMD_LOC))
 parser.add_argument("--submit", action="store_true", help="submit with sbatch")
 args = parser.parse_args()
+
+# iff the output directory is the default, let's ensure we have a location to 
+# put the scripts. only do this for the default location
+if args.outdir == str(DEFAULT_OUTPUT_DIR):
+    DEFAULT_OUTPUT_DIR.mkdir(exist_ok=True)
 
 # Open the Intake catalog and subset on the institution and subexperiment
 catalog = intake.open_esm_datastore("/gws/nopw/j04/snapsi/test-snapsi-catalog.json")
