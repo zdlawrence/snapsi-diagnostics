a28b790c3bda6ad1751d78f2a7c0c082473aea5f
Zachary Lawrence <4175294+zdlawrence@users.noreply.github.com>
2023-05-04 15:42:17 +0100

    Add capability to compile the final zonal mean datasets in the processed directories with --compile_complete

--- scripts/zdlawren/query_zmd_files.py
+++ scripts/zdlawren/query_zmd_files.py
@@ -11,11 +11,14 @@ args = parser.parse_args()
 
 dates = ["s20180125", "s20180208", "s20181213", "s20190108", "s20190829", "s20191001"]
 exps = ["nudged","control","free"]
+if (args.institution in ["UKMO"]):
+    exps += ["nudged-full","control-full"]
+
 dat_path = pathlib.Path("/gws/nopw/j04/snapsi/zmd/")
 paths = [dat_path / args.institution / f"{date}/{exp}" for date in dates for exp in exps]
 
 for path in paths:
-    if (path.is_dir() is False):
+    if path.is_dir() is False:
         print(f"{path} does not exist")
         continue
     nc_files = list(path.glob("*.nc"))
@@ -37,12 +40,12 @@ for path in paths:
 
         output_file = pathlib.Path(f"/gws/nopw/j04/snapsi/processed/{args.institution}/{experiment}/{init}/{args.institution}_{experiment}_{init}_zonalmeans.nc")
         
-        print(f"\t(compile_complete) Now compiling final dataset for {args.institution} {experiment} {init}")
-        if output_file.exists() is False:
+        print(f"\t(compile_complete=True) Now compiling final dataset for {args.institution} {experiment} {init}")
+        if (output_file.exists() is False) or (np.abs(output_file.stat().st_size/(1024*1024) - np.sum(fi_sizes)) >= 15):
             output_file.parent.mkdir(parents=True, exist_ok=True)
             mfds = xr.open_mfdataset(nc_files, combine="nested", concat_dim="member_id").load()
-            print(f"\t(compile_complete) Writing compiled dataset to {output_file}")
+            print(f"\t(compile_complete=True) Writing compiled dataset to {output_file}")
             mfds.to_netcdf(output_file)
         else:
-            print(f"\t(compile_complete) {output_file} already exists; skipping!")
+            print(f"\t(compile_complete=True) {output_file} already exists; skipping!")
        
