24854d2787fd51f2017ddfd9fdda39c400a46ff0
Zachary Lawrence <4175294+zdlawrence@users.noreply.github.com>
2023-05-04 15:40:38 +0100

    Add additional commandline args for setting job duration/mem allocation + additional guardrails

--- scripts/zdlawren/snapsi_zmd_genner.py
+++ scripts/zdlawren/snapsi_zmd_genner.py
@@ -13,13 +13,41 @@ am not very familiar with SLURM and how to pass specific args
 to scripts.
 
 Original Author: Z. D. Lawrence
-Last modified: 2023-04-25
+Last modified: 2023-05-04
 """
 
 import os
+import re
+import time
 import pathlib
 import argparse
 import intake
+
+def is_valid_duration(duration):
+    """ Check for a valid job duration string that 
+    will be accepted by SLURM. Format of HH:MM:SS
+    """
+    pattern = r"^([0-9]{2}):([0-9]{2}):([0-9]{2})$"
+    match = re.match(pattern, duration)
+    if match is None:
+        return False 
+    hours, mins, secs = int(match.group(1)), int(match.group(2)), int(match.group(3))
+    return (0 <= hours < 24) and (0 <= mins < 60) and (0 <= secs < 60)
+
+
+def is_valid_memsize(memsize):
+    """ Check for a valid memsize string that 
+    will be accepted by SLURM. Format of {num}G
+    to specify the number of gigabytes. num must 
+    be less than 100 (this is arbitrary).
+    """
+    pattern = r"^([0-9]+)G$"
+    match = re.match(pattern, memsize)
+    if match is None:
+        return False
+    memory_gb = int(match.group(1))
+    return memory_gb < 100
+
 
 USER_HOME = pathlib.Path("~/").expanduser()
 
@@ -37,7 +65,30 @@ parser.add_argument("--outdir", type=str, default=str(DEFAULT_OUTPUT_DIR))
 parser.add_argument("--python", type=str, default=str(DEFAULT_PYTHON_LOC))
 parser.add_argument("--zmd", type=str, default=str(DEFAULT_ZMD_LOC))
 parser.add_argument("--submit", action="store_true", help="submit with sbatch")
+parser.add_argument("--wait", type=int, default=5)
+parser.add_argument("--mem", type=str, default="20G", help="memory allocation for job in format of [num]G to specify the number of GB")
+parser.add_argument("--timelimit", type=str, default="06:00:00", help="duration of job in format of HH:MM:SS")
 args = parser.parse_args()
+
+# First make some checks that would stop the script from running
+# if there are issues with the commandline args
+if is_valid_duration(args.timelimit) is False:
+    msg = f"'{args.timelimit}' is not a valid timelimit; needs HH:MM:SS"
+    raise ValueError(msg)
+
+if is_valid_memsize(args.mem) is False:
+    msg = f"'{args.mem}' is not a valid memsize; needs [num]G where [num] is an int < 100"
+    raise ValueError(msg)
+
+valid_institutions = ["CNR-ISAC", "ECCC", "KMA", "SNU", "UKMO"]
+if args.institution not in valid_institutions:
+    msg = f"First arg '{args.institution}' must be one of {valid_institutions}"
+    raise ValueError(msg)
+
+valid_subexps = ["s20180125", "s20180208", "s20181213", "s20190108", "s20190829", "s20191001"]
+if args.subexperiment not in valid_subexps:
+    msg = f"Second arg '{args.subexperiment}' must be one of {valid_subexps}"
+    raise ValueError(msg)
 
 # iff the output directory is the default, let's ensure we have a location to 
 # put the scripts. only do this for the default location
@@ -62,8 +113,8 @@ for eid in experiment_ids:
     print(f"Generating {args.outdir}/{scripts_fi}")
     text_for_script = "#!/bin/bash\n\n"
     text_for_script += f"#SBATCH --job-name=\"{args.institution}_{args.subexperiment}_{eid}\"\n"
-    text_for_script += "#SBATCH --mem=32G\n"
-    text_for_script += "#SBATCH --time=04:00:00\n\n"
+    text_for_script += "#SBATCH --mem={args.mem}\n"
+    text_for_script += "#SBATCH --time={args.timelimit}\n\n"
     text_for_script += f"{args.python} -u {args.zmd} {args.institution} {args.subexperiment} {eid}\n"
 
     # Write string to file and chmod it for use
@@ -77,3 +128,4 @@ for eid in experiment_ids:
         cmd = f"sbatch {args.outdir}/{scripts_fi}"
         print(f"Submitting `{cmd}`")
         os.system(cmd)
+        time.sleep(args.wait)
